4e55a175030e4f3b7a062ecd8015be351eed1b1b
akai-keisanki <iago.ad@outlook.com>
2025-07-12 19:56:22 -0300

    feat: added `App` class and root route for the application.

--- app.py
+++ app.py
@@ -1,1 +1,40 @@
+import flask
+import sys
 
+class App (object):
+
+    _app : flask.Flask
+
+    def __init__ (self) -> None:
+
+        self._app = flask.Flask(__name__);
+
+    def set_routes (self) -> None:
+        """
+        App route setter
+        """
+
+        @self._app.route('/')
+        def root () -> None:
+
+            return 'The app is running.'
+
+    def start_apis (self) -> None:
+
+        ...
+    
+    def main (self) -> None:
+        """
+        App main function.
+
+        Executes `set_routes`, `start_apis` and runs the application.
+        """
+
+        self.set_routes()
+
+        self.start_apis()
+        
+        self._app.run(debug = sys.argv[-1] == 'debug')
+
+
+if __name__ == '__main__': App().main()
