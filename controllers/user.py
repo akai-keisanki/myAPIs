0a30163c4cbc330495c59fe3102fdad2ae8dc01a
AlvsDias <98051617+AlvsDias@users.noreply.github.com>
2025-08-16 20:56:59 -0300

    feat: add user controller routes for checking an user and creating an user

--- controllers/user.py
+++ controllers/user.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify
+from flask.globals import request
 from flask_sqlalchemy import SQLAlchemy
 
 from factory import db
@@ -13,3 +14,29 @@ def user_list ():
     response = [{'username': user.username, 'email': user.email} for user in users]
 
     return jsonify(response), 200
+
+@user_controller.route('/<int:id>', methods=['GET'])
+def user_id_get (id : int):
+
+    user = User.query.filter_by(id = id).first()
+    if not user: return f'User with id {id} not found!', 404
+
+    response = {'username': user.username, 'email': user.email}
+
+    return jsonify(response), 200
+
+@user_controller.route('/', methods=['POST'])
+def user_post ():
+
+    data = request.json()
+    if not all(key in data for key in ['username', 'email', 'password']): return 'Bad request!', 400
+
+    user = User(username=data['username'], email=data['email'], password=data['password'])
+
+    response = {'username': user.username, 'email': user.email, 'id': user.id}
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify(response), 201
+
