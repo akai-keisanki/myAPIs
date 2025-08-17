f13371fea7a1b56d86da45e30f2839e495b2b797
AlvsDias <98051617+AlvsDias@users.noreply.github.com>
2025-08-16 21:23:18 -0300

    feat: add user controller routes for deleting an user and modified error messaging

--- controllers/user.py
+++ controllers/user.py
@@ -11,7 +11,7 @@ user_controller = Blueprint('user_controller', __name__, url_prefix='/user')
 def user_list ():
 
     users = User.query.all()
-    response = [{'username': user.username, 'email': user.email} for user in users]
+    response = [{'id': user.id, 'username': user.username, 'email': user.email} for user in users]
 
     return jsonify(response), 200
 
@@ -19,24 +19,39 @@ def user_list ():
 def user_id_get (id : int):
 
     user = User.query.filter_by(id = id).first()
-    if not user: return f'User with id {id} not found!', 404
+    if not user: return {'message': f'User with id {id} not found!'}, 404
 
-    response = {'username': user.username, 'email': user.email}
+    response = {'id': user.id, 'username': user.username, 'email': user.email}
 
     return jsonify(response), 200
 
 @user_controller.route('/', methods=['POST'])
 def user_post ():
 
-    data = request.json()
-    if not all(key in data for key in ['username', 'email', 'password']): return 'Bad request!', 400
+    data = request.json
+    if not all(key in data for key in ['username', 'email', 'password']): return {'message': 'Bad request!'}, 400
 
     user = User(username=data['username'], email=data['email'], password=data['password'])
-
-    response = {'username': user.username, 'email': user.email, 'id': user.id}
 
     db.session.add(user)
     db.session.commit()
 
+    response = {'id': user.id, 'username': user.username, 'email': user.email}
+
     return jsonify(response), 201
 
+@user_controller.route('/<int:id>', methods=['DELETE'])
+def user_id_delete (id : int):
+
+    data = request.json
+    if 'password' not in data: return {'message': 'Bad request!'}, 400
+
+    user = User.query.filter_by(id = id).first()
+    if not user: return {'message': f'User with id {id} not found!'}, 404
+
+    if data['password'] != user.password: return {'message': f'Wrong password for user with id {id}!'}, 403
+
+    db.session.delete(user)
+    db.session.commit()
+
+    return {'message': f'User with id {id} successfully deleted!'}, 200
